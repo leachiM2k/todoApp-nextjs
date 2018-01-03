48f45d8aa8ce0c206bd547b3639421e797ae8ded
Michael Rotmanov <leachiM2k@leachiM2k.de>
2018-01-03 07:58:02 +0100

    Added some styling

---
+++ styles/button.js
@@ -0,0 +1,12 @@
+import css from 'styled-jsx/css';
+
+export const defaultButton = css`
+button {
+    border: 1px solid black;
+    border-radius: 0;
+    background: #1B325E;
+    color: white;
+    font-weight: bold;
+    margin: 5px;
+}
+`;
